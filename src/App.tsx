14dfaf8bc1721120030c29d8daf2945e13c75ca6
Profe Naomi <naomi.urrea94@gmail.com>
2026-03-12 11:35:18 -0300

    build: trigger redeploy and verify light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { QuizSetup } from './components/QuizSetup';
 import { QuizRunner } from './components/QuizRunner';
 import { LoginModal } from './components/LoginModal';
